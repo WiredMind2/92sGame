7009ebaa91409511502c79909f95ba64c31cc099
WiredMind2 <william@michaud.re>
2022-11-04 19:38:03 +0100

    Perlin Noise generator

--- mapGenerator.py
+++ mapGenerator.py
@@ -1,3 +1,8 @@
+import math
+import random
+
+from utils import Vector2
+
 class MapGenerator:
     def __init__(self):
         pass
@@ -41,3 +46,97 @@ class Chunk:
         """
 
         return self.data
+
+class Perlin:
+    """
+    Perlin Noise Generator => génération d'un array de valeurs aléatoires cohérentes
+    """
+    def __init__(self) :
+        self.perm1D = [2*random.random()-1 for _ in range(256)]
+        self.perm2D = [[(2*random.random()-1,2*random.random()-1) for x in range(256)] for y in range(256)]
+
+        self.fractal_factors = [(300, 1), (150, 2), (75, 4), (37.5, 8)]
+
+    def noise1D(self, p):
+        p0 = math.floor(p)
+        p1 = p0 + 1
+
+        t = p - p0
+        fade_t = self.fade(t)
+
+        g0 = self.grad1D(p0)
+        g1 = self.grad1D(p1)
+
+        return (1-fade_t)*g0*(p - p0) + fade_t*g1*(p - p1)
+
+    def fractalNoise1D(self, p, factors=5):
+        return sum(self.noise1D(p / i**2) / i**2 for i in range(1, factors+1))
+
+    def fade(self, t):
+        # t^3(t(t*6-15)+10)
+        return t*t*t*(t*(t*6 - 15) + 10)
+
+    def grad1D(self, p):
+        return self.perm1D[p & 256-1]
+
+    def noise2D(self, p):
+        p0 = math.floor(p)
+        p1 = p0 + (1, 0)
+        p2 = p0 + (0, 1)
+        p3 = p0 + (1, 1)
+
+        g0 = self.grad2D(p0)
+        g1 = self.grad2D(p1)
+        g2 = self.grad2D(p2)
+        g3 = self.grad2D(p3)
+
+        t0 = p.x - p0.x
+        fade_t0 = self.fade(t0)
+
+        t1 = p.y - p0.y
+        fade_t1 = self.fade(t1)
+
+        p0p1 = (1 - fade_t0) * (g0 @ (p - p0)) + fade_t0 * (g1 @ (p - p1))
+        p2p3 = (1 - fade_t0) * (g2 @ (p - p2)) + fade_t0 * (g3 @ (p - p3))
+
+        return (1 - fade_t1) * p0p1 + fade_t1 * p2p3
+
+    def fractalNoise2D(self, p, factors=5):
+        return sum(self.noise2D(p / i**2) / i**2 for i in range(1, factors+1))
+    
+    def grad2D(self, p):
+        return Vector2(*self.perm2D[p.x & 256-1][p.y & 256-1])
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import time
+
+    p = Perlin()
+
+    size = 50
+    freq = 10
+
+    # x = [i for i in range(size)]
+    # y = [2*p.noise1D(i/freq) for i in range(size)]
+
+    # plt.plot(x, y)
+
+    # fractaly = [2*p.fractalNoise1D(i, 5)*3 for i in range(size)]
+
+    # plt.plot(x, fractaly)
+
+    zoom = 1/8
+
+    noise = [[p.noise2D(Vector2(x, y)/freq/zoom) for x in range(size)] for y in range(size)]
+
+    plt.figure()
+    plt.imshow(noise, cmap='gray', interpolation='lanczos')
+
+
+    # fractalnoise = [[p.fractalNoise2D(Vector2(x, y)) for x in range(size)] for y in range(size)]
+
+    # plt.figure()
+    # plt.imshow(fractalnoise, cmap='gray', interpolation='lanczos')
+
+    plt.show()
--- utils.py
+++ utils.py
@@ -15,77 +15,110 @@ class Vector:
     
     def _map(self, func):
         # Interne - Renvoie un tuple de coordonnées après avoir map sur func(i, coord)
-        return tuple(map(func, enumerate(self.coords)))
+        return tuple(func(i, c) for i, c in enumerate(self.coords))
 
     def dim_check(self, vec, op):
         # Interne - Vérifie que self et vec soient comparables
-        if not isinstance(vec, Vector) or vec.dim != self.dim:
-            raise TypeError(f"unsupported operand type(s) for {op}: '{type(self)}' and '{type(vec)}'")
+        if isinstance(vec, Vector):
+            if vec.dim == self.dim:
+                return vec
+        elif isinstance(vec, (tuple, list)):
+            if len(vec) == self.dim:
+                return Vector(self.dim, *vec)
+            
+        raise TypeError(f"unsupported operand type(s) for {op}: '{type(self)}' and '{type(vec)}'")
 
     def get_dim(self, dim):
         # Renvoie la valeur correspondant à la dimension dim
         return self.coords[dim]
-    
+
+    @property
+    def x(self):
+        return self.get_dim(0)
+
+    @property
+    def y(self):
+        return self.get_dim(1)
+
+    @property
+    def z(self):
+        return self.get_dim(2)
+
     def __getitem__(self, dim):
         return self.coords[dim]
 
     def __setitem__(self, dim, val):
         self.coords[dim] = val
 
+    def __str__(self):
+        return f"Vector{self.dim}({', '.join(self.coords)})"
+
+    def __repr__(self):
+        return f"Vector{self.dim}({', '.join(self.coords)})"
+    
     def __add__(self, to):
         # vec1 + vec2 = (x1+x2, y1+y2)
-        self.dim_check(to, '+')
-        return Vector(self.dim, self._map(lambda i, c: c + to[i]))
+        to = self.dim_check(to, '+')
+        return Vector(self.dim, *self._map(lambda i, c: c + to[i]))
 
     def __sub__(self, to):
         # vec1 - vec2 = (x1-x2, y1-y2)
-        self.dim_check(to, '-')
+        to = self.dim_check(to, '-')
         return self.__add__(-to) # Yup i'm lazy
 
     def __mul__(self, n):
         # vec1 * n = (x*n, y*n)
-        return Vector2(self.dim, self._map(lambda i, c: c*n))
+        return Vector(self.dim, *self._map(lambda i, c: c*n))
+    
+    def __matmul__(self, to):
+        # vec1 @ vec2 => dot product
+        to = self.dim_check(to, '-')
+        return sum(self._map(lambda i, c: c*to[i]))
 
     def __truediv__(self, n):
         # vec1 / n = (x/n, y/n)
-        return Vector(self.dim, self._map(lambda i, c: c/n))
+        return Vector(self.dim, *self._map(lambda i, c: c/n))
 
     def __floordiv__(self, n):
         # vec1 // n = (x//n, y//n)
-        return Vector(self.dim, self._map(lambda i, c: c//n))
+        return Vector(self.dim, *self._map(lambda i, c: c//n))
     
     def __eq__(self, to):
         # vec1 == vec2
-        self.dim_check(to, '==')
+        to = self.dim_check(to, '==')
         return all(self._map(lambda i, c: c == to[i]))
     
     def __ne__(self, to):
         # vec1 != vec2
-        self.dim_check(to, '!=')
+        to = self.dim_check(to, '!=')
         return not self.__eq__(to)
     
     def __lt__(self, to):
         # vec1 < vec2 <=> |vec1|^2 < |vec2|^2
-        self.dim_check(to, '<')
+        to = self.dim_check(to, '<')
         return self.sqr_norm() < to.sqr_norm()
     
     def __le__(self, to):
         # vec1 <= vec2
-        self.dim_check(to, '<=')
+        to = self.dim_check(to, '<=')
         return self.__eq__(to) or self.__lt__(to)
 
     def __ge__(self, to):
         # vec1 >= vec2
-        self.dim_check(to, '>=')
+        to = self.dim_check(to, '>=')
         return not self.__lt__(to)
 
     def __neg__(self):
         # -vec = (-x, -y)
-        return Vector(self.dim, self._map(lambda i, c: -c))
+        return Vector(self.dim, *self._map(lambda i, c: -c))
     
     def __round__(self):
         # round(vec) = (round(x), round(y))
-        return Vector(self.dim, self._map(lambda i, c: round(c)))
+        return Vector(self.dim, *self._map(lambda i, c: round(c)))
+    
+    def __floor__(self):
+        # math.floor(vec) = (math.floor(x), math.floor(y))
+        return Vector(self.dim, *self._map(lambda i, c: math.floor(c)))
 
     def sqr_norm(self):
         # |vec|^2 => bcp plus rapide que norm() et permet toutes les comparaisons
